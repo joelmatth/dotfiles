e3449907630dedc23b22963162628c5375cda005
Joel Matthews <joel@joelmatth.com>
2019-05-23 15:42:14 +0100

    Remove unneeded Emacs shortcuts

--- init.el
+++ init.el
@@ -14,10 +14,6 @@
 
 (require 'evil)
 (evil-mode 1)
-(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
 (load-theme 'solarized-light t)
 
